3f4faaafdb6c851a3a888116f7b3d63c53b64bce
Shreya Pagaria <85084439+ShreyaPagaria@users.noreply.github.com>
2022-08-06 20:34:53 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,173 @@
+// Here you can find the dataset used for this website 
+// http://brahman.riggrodigital.com:8082/stream?listening-from-radio-garden=1655788562 
+
+const data = [
+    {
+      city: 'Delhi',
+      videos: [
+            'QZPqluJA0OY',
+            '2WaqdZTPiuM',
+            '_UlKnvXKUWE'    
+      ],
+      music:[
+        'http://streamasiacdn.atc-labs.com/radiomaharani.aac',
+        'http://uk2.internet-radio.com:8066/stream',
+        'http://brahman.riggrodigital.com:8082/stream'
+      ] 
+    },
+    {
+        city: 'Mumbai',
+        videos: [
+            '_Wb1ASZ4rBA',
+            '9Sxlstg6J3c',
+            'GDLOjbG97O4'
+    
+        ],
+        music:[
+          'https://prclive4.listenon.in/Freedom?listening-from-radio-garden=1655791521',
+          'http://namkeen.riggrodigital.com:8888/stream?listening-from-radio-garden=1655791593',
+          'https://prclive1.listenon.in/Hindi?listening-from-radio-garden=1655791614',
+          'https://prclive4.listenon.in/HipHop?listening-from-radio-garden=1655791633',
+        ] 
+      },
+      {
+        city: 'Jaipur',
+        videos: [
+            'jwZV8i60Sbs',
+            'q-QmlVSiKWg',
+            'oya79fnpX2M',
+            'JhqQVr6GB9k'
+    
+        ],
+        music:[
+            'http://live.radiotaj.com:9484/;stream.mp3',
+            'http://streamasiacdn.atc-labs.com/jaipurradio.aac',
+            'http://node-27.zeno.fm/csw7fv7sz1zuv',
+          
+        ] 
+      },
+      {
+        city: 'Guwahati',
+        videos: [
+            '1QxXnkOokN8',
+            'Bba_hM0y8H8',
+            'gt8V_cN1gw0',
+            
+        ],
+        music:[
+          'http://103.95.48.18:8080/;?listening-from-radio-garden=1655791834',
+          ''
+        ] 
+      },
+      {
+        city: 'Kolkata',
+        videos: [
+            'usx1RGyFQCk',
+            'AkutbKoYpdE',
+            'QOKZYt7a0pw'
+    
+        ],
+        music:[
+            'https://stream.zeno.fm/15wnypt6by8uv',
+            'https://carina.streamerr.co:8114/stream',
+            'https://stream.zeno.fm/9msu0vbxezzuv',
+        ] 
+      },
+      {
+        city: 'Goa',
+        videos: [
+            'az7A4tJffyE',
+            'MoAygX3sTxg',
+            '8XJCrAa9QaE'
+    
+        ],
+        music:[
+          
+        ] 
+      },
+      {
+        city: 'Pune',
+        videos: [
+            'O2BR2WgBJUs',
+            'D_zKaUC5U4U',
+            'PO9MI8h52bY',
+            'Hz_RoiwdBjg'
+    
+        ],
+        music:[
+          'http://janus.shoutca.st:8112/stream3/;',
+          'https://eu10.fastcast4u.com:5045/stream',
+          'http://janus.shoutca.st:8112/;'
+        ] 
+      },
+      {
+        city: 'Chandigarh',
+        videos: [
+          'QXwr8IKp4nk',
+          'VO9K-Ve4gN4',
+          '1NC8dXxuZ4g'
+    
+        ],
+        music:[
+          'http://149.56.195.94:8322/bolpunjabiradio',
+          'https://eu2.fastcast4u.com/proxy/hindiradio'
+        ] 
+      },
+      {
+        city: 'Kochi',
+        videos: [
+          'DxJxfSnRvWU',
+          'GW9ZFZwi-YU',
+          '8dz9m77EJ60'
+    
+        ],
+        music:[
+          'https://stream.zeno.fm/xd6trx3e84zuv',
+          'http://108.170.51.230:8010/;',
+          'http://titan.shoutca.st:8647/stream'
+        ] 
+      },
+      {
+        city: 'Bhubaneshwar',
+        videos: [
+          'OzdOeQeiAg8',
+          '6WUdVxzlSRo',
+          '--cPrp97IH8'
+    
+        ],
+        music:[
+          'https://streams.radiojajabara.com/hdradio_naibedya.mp3',
+          'http://178.33.135.245:4362/;',
+          'http://104.243.42.94:8574/rabh8989uba'
+        ] 
+      },
+      {
+        city: 'Ahemdabad',
+        videos: [
+          'O2DzpNjbdMo',
+          'um3jQ2I5Xqs',
+          'saG6xPEelb0'
+    
+        ],
+        music:[
+          'http://s9.voscast.com:7402/;',
+          'https://prclive4.listenon.in/Gujarati',
+          'https://sayajifm.radioca.st/stream'
+        ] 
+      },
+    
+    {
+        city: 'Manali',
+        videos: [
+          'MN1a5q8Qq_U',
+          'zQiOpjSWFw4',
+          'QaVnSb3zvRE'
+    
+        ],
+        music:[
+          'http://himachaligeet.com:8000/radio.mp3'
+        ] 
+      },
+  ]
+
+// SOME LINKS MIGHT THROW AN ERROR.
